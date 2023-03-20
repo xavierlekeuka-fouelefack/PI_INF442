7cfa652f66cb889d5f73be834928339e9fa7fdcd
xavierlekeuka-fouelefack <116391760+xavierlekeuka-fouelefack@users.noreply.github.com>
2023-03-20 13:46:18 +0100

    Update visual.py

--- visual.py
+++ visual.py
@@ -5,15 +5,13 @@ import pandas as pd
 
 df_2008_01 = pd.read_csv('data/synop.200801.csv', delimiter =';')
 
-#print(df_2008_01.head())
-
-
-#df_2008_01 = df_2008_01.loc[df_2008_01['numer_sta']==7149].copy().reset_index(drop = True)
-#print(df_2008_01)
+print(df_2008_01.head())
+df_2008_01_orly = df_2008_01.loc[df_2008_01['numer_sta']==7149].copy().reset_index(drop = True)
+print(df_2008_01_orly.head())
 
 data_power = pd.read_csv('data/household_power_consumption.txt', delimiter = ';', low_memory = False)
 
-print(data_power)
+print(data_power.head())
 
 
 
