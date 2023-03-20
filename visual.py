39204356f9868900b829f521bf270ebf0eec8789
xavierlekeuka-fouelefack <116391760+xavierlekeuka-fouelefack@users.noreply.github.com>
2023-03-20 13:44:15 +0100

    Add files via upload

---
+++ visual.py
@@ -0,0 +1,20 @@
+
+import numpy as numpy
+import pandas as pd
+
+
+df_2008_01 = pd.read_csv('data/synop.200801.csv', delimiter =';')
+
+#print(df_2008_01.head())
+
+
+#df_2008_01 = df_2008_01.loc[df_2008_01['numer_sta']==7149].copy().reset_index(drop = True)
+#print(df_2008_01)
+
+data_power = pd.read_csv('data/household_power_consumption.txt', delimiter = ';', low_memory = False)
+
+print(data_power)
+
+
+
+
